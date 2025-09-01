99c31b8e7feab796e65b76899085fcde00cfa953
ehqlsms1004 <ehqlsms1004@gmail.com>
2025-09-01 15:21:42 +0900

    20250901

---
+++ 15.클로저와데코레이터/wrapper.py
@@ -0,0 +1,15 @@
+# wrapper.py
+
+def mul(m):
+    def wrapper(n):
+        return m*n
+    return wrapper
+
+if __name__ == '__main__':
+    mul3 = mul(3)
+    mul5 = mul(5)
+
+    # print(mul3(10))
+    # print(mul5(10))
+    print(mul(3)(10))
+    print(mul(5)(10))
